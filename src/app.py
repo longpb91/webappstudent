585bef729a88741511c833b4378bdb3b66d82798
= <longpb.91@gmail.com>
2022-01-08 15:46:09 +0700

    big update

--- src/app.py
+++ src/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import os
+import traceback
 
-# from src.models import Students
+from src.models import Students
 
 # from dotenv import load_dotenv
 #
@@ -25,24 +26,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class Students(db.Model):
-    __tablename__ = 'students'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200))
-    class_ = db.Column(db.String(200))
-    subject = db.Column(db.Text())
-
-    def __init__(self, id, name, class_, subject):
-        self.id = id
-        self.name = name
-        self.class_ = class_
-        self.subject = subject
-
-
 @app.route('/')
 def main_page():
-    return render_template('test_mainpage.html')
+    return render_template('mainpage.html')
 
 
 @app.route('/students', methods=['GET', 'POST'])
@@ -73,7 +59,8 @@ def add_students():
             return render_template('students.html', message='You have already submitted.')
         return render_template("students.html")
     except Exception as e:
-        return(str(e))
+        # return(str(e))
+        return traceback.print_exc()
 
 
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,18 +1,22 @@
-# from enum import unique
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+
+db = SQLAlchemy(app)
 
 class Students(db.Model):
     __tablename__ = 'students'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
-    clas = db.Column(db.String(200))
+    class_ = db.Column(db.String(200))
     subject = db.Column(db.Text())
 
-    def __init__(self, id, name, clas, subject):
+    def __init__(self, id, name, class_, subject):
         self.id = id
         self.name = name
-        self.clas = clas
+        self.class_ = class_
         self.subject = subject
 
         
