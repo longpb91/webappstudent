1b70a6e80234cc4c2c43823d7b50039e9e279e7d
= <longpb.91@gmail.com>
2022-01-10 13:34:28 +0700

    Big update in html

--- src/app.py
+++ src/app.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import os
 import traceback
 
 from src.models import Students
 
-# from dotenv import load_dotenv
-#
-#
-# load_dotenv()
+from dotenv import load_dotenv
 
-app = Flask(__name__)
+
+# load_dotenv()
+#
+# app = Flask(__name__)
 
 # ENV = 'dev'
 ENV = 'prod'
@@ -19,7 +19,7 @@ if ENV == 'dev':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL_DEV')
 else:
-    app.debug = False
+    app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL_PROD')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -41,13 +41,18 @@ def add_students():
             subject = request.form.getlist('subject')
 
             if id == "" or name == "" or class_ == "" or subject == "":
-                return render_template("students.html", message='Please enter required fields.')
+                error = "Please enter required fields."
+                return render_template("students.html", error=error)
 
             if len(id) < 5 or len(id) > 5:
-                return render_template("students.html", message='The id must contain 12 numbers.')
+                error = "The id must contain 12 numbers."
+                return render_template("students.html", error=error)
+                # return render_template("students.html", message='The id must contain 12 numbers.')
 
             if id.isdigit() is False:
-                return render_template("students.html", message='Only enter number!')
+                error = "Only enter number in Student ID"
+                return render_template("students.html", error=error)
+                # return render_template("students.html", message='Only enter number!')
 
             if db.session.query(Students).filter(Students.id == id).count() == 0:
                 data_students = Students(id, name, class_, subject)
@@ -55,15 +60,22 @@ def add_students():
                 db.session.commit()
 
                 # return "Student with ID={}".format(data_students.id)
-                return render_template("success.html")
-            return render_template('students.html', message='You have already submitted.')
+                # return render_template("success.html")
+                msg = 'Successfully! Thank you for your information.'
+                return render_template('students.html', msg=msg)
+            error = 'You have already submitted.'
+            return render_template('students.html', error=error)
         return render_template("students.html")
     except Exception as e:
         # return(str(e))
         return traceback.print_exc()
 
 
+@app.route('/students-info')
+def get_students():
+    return render_template('studentsinfo.html')
+
+
 if __name__ == '__main__':
     # app.debug = True
     app.run()
-
