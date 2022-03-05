b8b648413d59224aff4269bd9765fcd565812cca
= <longpb.91@gmail.com>
2022-03-05 23:11:11 +0700

    test

--- src/app.py
+++ src/app.py
@@ -22,8 +22,8 @@ if ENV == 'dev':
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL_DEV')
 else:
     app.debug = True
-    # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL_PROD')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL_PROD')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL_PROD')
+    # app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL_PROD')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
